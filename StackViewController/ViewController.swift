003a17f731dedf100c0d418a5b15b6af1d40a52f
Indragie Karunaratne <i@indragie.com>
2016-04-11 13:39:30 -0600

    Initial Commit

---
+++ StackViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewController
+//
+//  Created by Indragie Karunaratne on 2016-04-11.
+//  Copyright © 2016 Seed Platform, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
