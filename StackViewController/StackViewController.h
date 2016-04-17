65e95f43ef521224e0968d6f96e3cc5063c8d8ee
Indragie Karunaratne <i@indragie.com>
2016-04-16 23:35:16 -0600

    Separate into framework and app targets. Close #6

---
+++ StackViewController/StackViewController.h
@@ -0,0 +1,19 @@
+//
+//  StackViewController.h
+//  StackViewController
+//
+//  Created by Indragie Karunaratne on 2016-04-16.
+//  Copyright © 2016 Seed Platform, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StackViewController.
+FOUNDATION_EXPORT double StackViewControllerVersionNumber;
+
+//! Project version string for StackViewController.
+FOUNDATION_EXPORT const unsigned char StackViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackViewController/PublicHeader.h>
+
+
