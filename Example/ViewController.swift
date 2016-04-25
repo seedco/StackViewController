3e7b685c716aefb05ee6f515f51b0a7b6e880dda
Indragie Karunaratne <i@indragie.com>
2016-04-24 19:16:40 -0600

    Remove Main.storyboard

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import StackViewController
 
 class ViewController: UIViewController {
+    private let stackViewController: StackViewController
+    
+    init() {
+        stackViewController = StackViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = UIView(frame: CGRectZero)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
