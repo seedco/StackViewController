06cdf4ca3f2d89a32abd94eef85952503f66eb15
Indragie Karunaratne <i@indragie.com>
2016-04-24 19:56:10 -0600

    Add support for specifying insets in UIView.activateSuperviewHuggingConstraints()

--- StackViewController/UIView+Seed.swift
+++ StackViewController/UIView+Seed.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 extension UIView {
-    public func activateSuperviewHuggingConstraints() -> [NSLayoutConstraint] {
+    public func activateSuperviewHuggingConstraints(insets insets: UIEdgeInsets = UIEdgeInsetsZero) -> [NSLayoutConstraint] {
         let views = ["view": self]
-        var constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: [], metrics: nil, views: views)
-        constraints.appendContentsOf(NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: [], metrics: nil, views: views))
+        let metrics = ["top": insets.top, "left": insets.left, "bottom": insets.bottom, "right": insets.right]
+        var constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[view]-right-|", options: [], metrics: metrics, views: views)
+        constraints.appendContentsOf(NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[view]-bottom-|", options: [], metrics: metrics, views: views))
         NSLayoutConstraint.activateConstraints(constraints)
         return constraints
     }
